2b14e39d1371bb6214883eb0cac79aa882ccfccf
xieyuhang <13451213869@163.com>
2025-05-14 10:53:46 +0800

    fix: missing commits

--- packages/scripts/src/expose.ts
+++ packages/scripts/src/expose.ts
@@ -1,6 +1,8 @@
 import type { Configuration } from 'webpack'
 import type { ProjectConfig } from './compile'
 
+export { default as HtmlWebpackPlugin } from 'html-webpack-plugin'
+
 export function defineProjectConfig(config: ProjectConfig) {
   return config
 }
@@ -9,6 +11,7 @@ export function removeEntryConfig(webpackConfig: Configuration) {
   delete webpackConfig.entry
 
   webpackConfig.plugins = webpackConfig.plugins!.filter((plugin) => {
+    if (!plugin) return false
     if (!plugin.constructor) return true
     return plugin.constructor.name !== 'HtmlWebpackPlugin'
   })
